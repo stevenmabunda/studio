6cb14d6cd228abce14b6c82facd2775cb99fc7c2
Steven Mabunda <steven@s2multimedia.net>
2025-07-10 10:11:24 +0000

    adjust the stacked icons position, they're too close to the edge of the

--- src/components/video-post.tsx
+++ src/components/video-post.tsx
@@ -4,7 +4,6 @@
 import { PostType } from '@/lib/data';
 import Link from 'next/link';
 import { useRef } from 'react';
-import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Button } from './ui/button';
 import { Heart, MessageCircle, Repeat, Share2, Bookmark } from 'lucide-react';
 import { cn } from '@/lib/utils';
@@ -37,7 +36,7 @@ export function VideoPost({ post }: VideoPostProps) {
       
       <div className="absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-t from-black/60 to-transparent pointer-events-none" />
 
-      <div className="absolute bottom-0 left-0 right-0 p-4 z-10 text-white">
+      <div className="absolute bottom-0 left-0 right-0 p-6 z-10 text-white">
         <div className="flex-1 space-y-2 min-w-0">
           <Link
             href={`/profile/${post.authorId}`}
@@ -50,7 +49,7 @@ export function VideoPost({ post }: VideoPostProps) {
         </div>
       </div>
 
-      <div className="absolute bottom-20 right-2 z-10 flex flex-col gap-4">
+      <div className="absolute bottom-24 right-4 z-10 flex flex-col gap-4">
         <div className="flex flex-col items-center gap-1 text-white">
             <Button variant="ghost" size="icon" className="h-12 w-12 rounded-full bg-black/30 hover:bg-black/50">
                 <Heart className="h-7 w-7" />
