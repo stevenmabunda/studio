27aba86c5430d3702aebb6d2ba2997cd3ac1d8fd
Steven Mabunda <steven@s2multimedia.net>
2025-08-12 13:36:44 +0000

    I've added the following favicons on the public folder, see pictyure, pl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,20 @@ import { TabProvider } from '@/contexts/tab-context';
 export const metadata: Metadata = {
   title: 'BHOLO',
   description: 'A football-focused social media app.',
+  icons: {
+    icon: [
+      { url: '/favicon_io/favicon.ico', sizes: 'any' },
+      { url: '/favicon_io/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+      { url: '/favicon_io/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+    ],
+    apple: [
+      { url: '/favicon_io/apple-touch-icon.png', type: 'image/png', sizes: '180x180' },
+    ],
+    other: [
+       { rel: 'icon', type: 'image/png', sizes: '192x192', url: '/favicon_io/android-chrome-192x192.png' },
+       { rel: 'icon', type: 'image/png', sizes: '512x512', url: '/favicon_io/android-chrome-512x512.png' },
+    ]
+  }
 };
 
 export default function RootLayout({
